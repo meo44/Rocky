4b8baa2ea10c83889b332ad4bc9bc68dbe870ce3
meo44 <elinah8893@gmail.com>
2024-02-22 20:33:37 -0800

    game done

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Hello")
+let username = ''
+username = prompt("What is your name")
+console.log(username)
+const intro = document.querySelector('#intro');
+intro.textContent = "Hello " + username
+function botMove (move) {
+    if (move == "rock") {
+        return "paper"
+    }
+    if (move == "paper") {
+        return "scissors"
+    }
+    if (move == "scissors") {
+        return "rock"
+    }
+}
+function playerMove (move) {
+    const botText = document.querySelector('#botText');
+    let decide = botMove(move)
+    botText.textContent = decide
+    console.log(decide)
+    const botThought = document.querySelector('#botThought');
+    botThought.src = "images/" + decide + ".jpg"
+    alert( username + " LOST")
+}
