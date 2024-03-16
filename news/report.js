cd5b278e054c3529b3eef714c33a55cdf4a6c0b0
meo44 <elinah8893@gmail.com>
2024-03-15 17:45:15 -0700

    news looked better

---
+++ news/report.js
@@ -0,0 +1,19 @@
+function search() {
+    document.getElementById("find").classList.toggle("show");
+  }
+  
+  function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("input");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("find");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
